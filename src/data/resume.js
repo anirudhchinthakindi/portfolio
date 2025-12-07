a216b1f9478cff1c9cb95b15fac7d04d09ca6c1c
anirudhchinthakindigit config --global user.namegit config --global user.name anirudhchinthakindi <ankchinta@gmail.com>
2025-12-07 15:33:55 -0500

    DATA Volunteer Update

--- src/data/resume.js
+++ src/data/resume.js
@@ -141,7 +141,7 @@ export const resumeData = {
             role: "Friendly Visitor and HUC Assistant",
             organization: "UVA Health",
             location: "Charlottesville, VA",
-            date: "2025 - Present",
+            date: "Sep 2025 - Present",
             details: "Positively impact patient experience by providing companionship to patients, reviewing hospital resources, and assisting staff via receptionist/clerical duties at nurses' station."
         },
         /*
@@ -153,6 +153,13 @@ export const resumeData = {
             details: "Developed and led a free STEM tutoring program for 7 underserved middle school students. Conducted 70+ sessions and improved students' STEM grades by an average of 12% over 4 semesters."
         },
         */
+        {
+            role: "Volunteer",
+            organization: "Dementia Art Therapy Alliance @ The Colonades",
+            location: "Charlottesville, VA",
+            date: "Nov 2025 - Present",
+            details: "Lead art-therapy activities for residents with dementia to support emotional well-being and cognitive engagement. Build supportive relationships and adapt activities to individual needs. Collaborate with staff and the Dementia Art Therapy Alliance to create inclusive, calming, and meaningful experiences."
+        },
         {
             role: "Chantilly Group Home Volunteer",
             organization: "Sunrise Community of Virginia",
